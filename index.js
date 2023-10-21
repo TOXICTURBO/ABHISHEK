f10e966d367d27149c6f8208c3d62096f5fcd8b8
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2023-10-21 15:52:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ABHISHEK-SER', {
+say('Toxic-Alexa_V4', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @ABHISHEK SURESH._`, {
+say(`'${name}' By @TurboMods._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
